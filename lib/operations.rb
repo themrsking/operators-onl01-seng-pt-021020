e834678f4d33efd6fea108b1e67fd472cca47d25
Stefani King <themrsking@gmail.com>
2020-02-20 08:18:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
+require 'pry'
 def unsafe?(speed)
-
+# binding.pry
+if speed > 60
+  true
+  elsif speed < 40 
+  true
+  elsif speed >= 40 && speed <= 60
+  false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+  #{conditional}? 'true/truthy' : 'false/falsey'
 end
-	
+(speed >= 40 && speed <= 60)? false : true
+
 
 
